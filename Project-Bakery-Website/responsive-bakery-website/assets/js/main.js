f425c4c928c904fc8bf5fd0aee0d5e1d851e4c8f
abdelfatah-kfs <abdelfatahabdelhamed.kfs@gmail.com>
2024-06-29 14:39:28 +0300

    Update main.js

--- Project-Bakery-Website/responsive-bakery-website/assets/js/main.js
+++ Project-Bakery-Website/responsive-bakery-website/assets/js/main.js
@@ -1,13 +1,13 @@
-    (function(d, w, c) {
-        w.ChatraID = 'CeSgY5qHiiJe2i5Qx';
-        var s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = 'https://call.chatra.io/chatra.js';
-        if (d.head) d.head.appendChild(s);
-    })(document, window, 'Chatra');
+(function(d, w, c) {
+    w.ChatraID = 'CeSgY5qHiiJe2i5Qx';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
@@ -87,15 +87,15 @@ const sr = ScrollReveal({
     // reset: true, // Animations repeat
 })
   
-sr.reveal(.home__data, .about__img, .about__data, .visit__data)
+sr.reveal(`.home__data, .about__img, .about__data, .visit__data`)
 
-sr.reveal(.home__image, .footer__img-1, .footer__img-2, { rotate: {z: -15} })
-sr.reveal(.home__bread, .about__bread, { rotate: {z: 15} })
-sr.reveal(.home__footer, { scale: 1, origin: 'bottom' })
+sr.reveal(`.home__image, .footer__img-1, .footer__img-2`, { rotate: {z: -15} })
+sr.reveal(`.home__bread, .about__bread`, { rotate: {z: 15} })
+sr.reveal(`.home__footer`, { scale: 1, origin: 'bottom' })
 
-sr.reveal(.new__card:nth-child(1) img, { rotate: {z: -30}, distance: 0 })
-sr.reveal(.new__card:nth-child(2) img, { rotate: {z: 15}, distance: 0, delay: 600 })
-sr.reveal(.new__card:nth-child(3) img, { rotate: {z: -30}, distance: 0, delay: 900 })
+sr.reveal(`.new__card:nth-child(1) img`, { rotate: {z: -30}, distance: 0 })
+sr.reveal(`.new__card:nth-child(2) img`, { rotate: {z: 15}, distance: 0, delay: 600 })
+sr.reveal(`.new__card:nth-child(3) img`, { rotate: {z: -30}, distance: 0, delay: 900 })
 
-sr.reveal(.footer__container, { scale: 1 })
-sr.reveal(.favorite__card img, { interval: 100, rotate: {z: 15}, distance: 0 })
+sr.reveal(`.footer__container`, { scale: 1 })
+sr.reveal(`.favorite__card img`, { interval: 100, rotate: {z: 15}, distance: 0 })
