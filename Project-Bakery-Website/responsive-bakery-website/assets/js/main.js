b19c4fb05c850de445c29baa5a8a1e2d31ed5826
abdelfatah-kfs <abdelfatahabdelhamed.kfs@gmail.com>
2024-06-29 14:40:43 +0300

    Update main.js

--- Project-Bakery-Website/responsive-bakery-website/assets/js/main.js
+++ Project-Bakery-Website/responsive-bakery-website/assets/js/main.js
@@ -1,13 +1,4 @@
-(function(d, w, c) {
-    w.ChatraID = 'CeSgY5qHiiJe2i5Qx';
-    var s = d.createElement('script');
-    w[c] = w[c] || function() {
-        (w[c].q = w[c].q || []).push(arguments);
-    };
-    s.async = true;
-    s.src = 'https://call.chatra.io/chatra.js';
-    if (d.head) d.head.appendChild(s);
-})(document, window, 'Chatra');
+
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
